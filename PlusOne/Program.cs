f43e6e89db89cd8ae3262c2cbb6f83b67d2be55c
AbbosjonIkromov <abbos6562@gmail.com>
2026-01-01 23:20:58 +0500

    Solve 0066 Plus One

--- PlusOne/Program.cs
+++ PlusOne/Program.cs
@@ -4,7 +4,18 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var solution = new Solution();
+
+        Test(solution, new int[] { 1, 2, 3 }); // [1, 2, 4]
+        Test(solution, new int[] { 4, 3, 2, 1 }); // [4, 3, 2, 2]
+        Test(solution, new int[] { 9, 9, 9 }); // [1, 0]
+
+    }
+
+    static void Test(Solution solution, int[] digits)
+    {
+        var result = solution.PlusOne(digits);
+        Console.WriteLine($"[{string.Join(", ", result)}]");
     }
 }
 
@@ -13,6 +24,20 @@ public class Solution
 {
     public int[] PlusOne(int[] digits)
     {
-        return new int[] { };
+        var lenght = digits.Length;
+
+        for (int i = lenght - 1; i >= 0; i--)
+        {
+            if (digits[i] < 9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        var result = new int[lenght + 1];
+        result[0] = 1;
+        return result;
     }
 }
