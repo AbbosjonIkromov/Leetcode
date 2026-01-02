16487ae139f34b7c0f931e35c5705c8318d30975
AbbosjonIkromov <abbos6562@gmail.com>
2026-01-02 16:22:10 +0500

    Solve 0961-RepeatedElementArray

---
+++ RepeatedElementArray/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace RepeatedElementArray;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var solution = new Solution();
+        Test(solution, new int[] { 1, 2, 3, 3 });
+        Test(solution, new int[] { 2, 1, 2, 5, 3, 2 });
+        Test(solution, new int[] { 5, 1, 5, 2, 5, 3, 5, 4 });
+    }
+
+    static void Test(Solution solution, int[] nums)
+    {
+        var result = solution.RepeatedNTimes(nums);
+        Console.WriteLine($"[{string.Join(", ", result)}]");
+    }
+}
+
+public class Solution
+{
+    public int RepeatedNTimes(int[] nums)
+    {
+        for(int i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i] == nums[i + 1])
+                return nums[i];
+
+            if(i + 2 < nums.Length && nums[i] == nums[i + 2])
+                return nums[i];
+        }
+        return nums[0];
+    }
+}
