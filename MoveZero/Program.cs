2bde0f5191e9d9cd084e2fea7a449a174b531ecf
AbbosjonIkromov <abbos6562@gmail.com>
2026-02-02 22:34:15 +0500

    Solve MoveZero

---
+++ MoveZero/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace MoveZero;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        Test(solution, new int[] { 0, 1, 0, 3, 12 }); // output [1,3,12,0,0]
+        Test(solution, new int[] { 0 }); // output [0]
+
+    }
+    static void Test(Solution solution, int[] nums)
+    {
+        solution.MoveZeroes(nums);
+        Console.WriteLine($"[{string.Join(",", nums)}]");
+    }
+}
+
+public class Solution
+{
+    public void MoveZeroes(int[] nums)
+    {
+        int lastNonZeroFoundAt = 0;
+        for (int current = 0; current < nums.Length; current++)
+        {
+            if (nums[current] != 0)
+            {
+                // Swap elements
+                int temp = nums[lastNonZeroFoundAt];
+                nums[lastNonZeroFoundAt] = nums[current];
+                nums[current] = temp;
+                lastNonZeroFoundAt++;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -6,6 +6,6 @@
 
 ## Progress
 
-* Easy: 32
+* Easy: 33
 * Medium: 0
 * Hard: 0
