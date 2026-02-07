d83f0fc333f12d161e818e4145df080bed4270de
AbbosjonIkromov <abbos6562@gmail.com>
2026-02-07 23:01:03 +0500

    Solve DetectCapital

---
+++ DetectCapital/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace DetectCapital;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        Test(solution, "USA"); // output true
+        Test(solution, "FlaG"); // output false
+        Test(solution, "leetcode"); // output true
+    }
+
+    static void Test(Solution solution, string word)
+    {
+        var result = solution.DetectCapitalUse(word);
+        Console.WriteLine($"[{result}]");
+    }
+}
+
+
+public class Solution
+{
+    public bool DetectCapitalUse(string word)
+    {
+        if (word.All(char.IsUpper) || word.All(char.IsLower))
+            return true;
+
+        return char.IsUpper(word[0]) && word.Skip(1).All(char.IsLower);
+    }
+}
