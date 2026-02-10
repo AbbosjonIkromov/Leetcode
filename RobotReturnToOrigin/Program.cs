e1cd7cf466a7d796742d0452b5dd5abcc8805c2b
AbbosjonIkromov <abbos6562@gmail.com>
2026-02-10 21:40:09 +0500

    Solve RobotReturnToOrigin

---
+++ RobotReturnToOrigin/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace RobotReturnToOrigin;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        Test(solution, "UD"); // output true
+        Test(solution, "LL"); // output false
+    }
+
+    static void Test(Solution solution, string moves)
+    {
+        var result = solution.JudgeCircle(moves);
+        Console.WriteLine($"[{result}]");
+    }
+}
+
+
+public class Solution
+{
+    public bool JudgeCircle(string moves)
+    {
+        int x = 0, y = 0;
+        foreach (char move in moves)
+        {
+            switch (move)
+            {
+                case 'U':
+                    y++;
+                    break;
+                case 'D':
+                    y--;
+                    break;
+                case 'L':
+                    x--;
+                    break;
+                case 'R':
+                    x++;
+                    break;
+            }
+        }
+        return x == 0 && y == 0;
+    }
+}
