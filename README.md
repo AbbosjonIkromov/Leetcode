662893fb0302a2a744a64dada0f10dcd78be0d1f
AbbosjonIkromov <abbos6562@gmail.com>
2026-02-11 21:32:14 +0500

    Solve ToLowerCase

--- README.md
+++ README.md
@@ -6,6 +6,6 @@
 
 ## Progress
 
-* Easy: 41
+* Easy: 42
 * Medium: 0
 * Hard: 0
---
+++ ToLowerCase/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ToLowerCase;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+
+        Test(solution, "Hello"); // output "hello"
+        Test(solution, "here"); // output "here"
+        Test(solution, "LOVELY"); // output "lovely"    
+    }
+
+    static void Test(Solution solution, string s)
+    {
+        var result = solution.ToLowerCase(s);
+        Console.WriteLine($"[{result}]");
+    }
+}
+
+
+public class Solution
+{
+    public string ToLowerCase(string s)
+    {
+        var chars = s.ToCharArray();
+
+        for(int i = 0; i < s.Length; i++)
+        {
+            if (chars[i] >= 'A' && chars[i] <= 'Z')
+                chars[i] = (char)(chars[i] + 32);
+        }
+
+        return new string(chars);
+    }
+}
