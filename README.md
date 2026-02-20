050f3fcbe0e6e2eb485056c3dbb3beafeab1cf8c
AbbosjonIkromov <abbos6562@gmail.com>
2026-02-20 23:11:16 +0500

    Solve ValidPalindromII

--- README.md
+++ README.md
@@ -6,6 +6,6 @@
 
 ## Progress
 
-* Easy: 50
+* Easy: 51
 * Medium: 0
 * Hard: 0
---
+++ ValidPalindromII/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ValidPalindromII;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        Test(solution, "aba"); // true
+        Test(solution, "abca"); // true
+        Test(solution, "abc"); // falseS
+    }
+
+    static void Test(Solution solution, string s)
+    {
+        var ressult = solution.ValidPalindrome(s);
+        Console.WriteLine($"[{ressult}]");
+    }
+}
+public class Solution
+{
+    public bool ValidPalindrome(string s)
+    {
+        int left = 0;
+        int right = s.Length - 1;
+        while (left < right)
+        {
+            if (s[left] != s[right])
+            {
+                return IsPalindrome(s, left + 1, right) || IsPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    private bool IsPalindrome(string s, int left, int right)
+    {
+        while (left < right)
+        {
+            if (s[left] != s[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
