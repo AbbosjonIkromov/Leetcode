5fa8fa41a2789cd29372b35bb2693f273920cf3d
AbbosjonIkromov <abbos6562@gmail.com>
2026-03-07 21:40:30 +0500

    Solve PowerOfThreee

---
+++ PowerOfThree/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace PowerOfThree;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        Test(solution, 27); // Output: [true]
+        Test(solution, 0);  // Output: [false]
+        Test(solution, 9);  // Output: [true]
+    }
+
+    static void Test(Solution solution, int n)
+    {
+        var result = solution.IsPowerOfThree(n);
+        Console.WriteLine($"[{result}]");
+    }
+}
+
+
+public class Solution
+{
+    public bool IsPowerOfThree(int n)
+    {
+        if (n <= 0) return false;
+
+        while (n % 3 == 0)
+        {
+            n /= 3;
+        }
+
+        return n == 1;
+    }
+}
