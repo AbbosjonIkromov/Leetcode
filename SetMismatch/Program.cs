735918f8c890798473b1e37d9609417803684f12
AbbosjonIkromov <abbos6562@gmail.com>
2026-03-13 09:24:59 +0500

    Solve SetMismatch

---
+++ SetMismatch/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace SetMismatch;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        Test(solution, new int[] { 1, 2, 2, 4 }); // Output: [2, 3]
+        Test(solution, new int[] { 1, 1 });       // Output: [1, 2]
+    }
+
+    static void Test(Solution solution, int[] nums)
+    {
+        var result = solution.FindErrorNums(nums);
+        Console.WriteLine($"[{string.Join(", ", result)}]");
+    }
+}
+
+
+public class Solution
+{
+    public int[] FindErrorNums(int[] nums)
+    {
+        HashSet<int> set = new HashSet<int>();
+        int duplicate = -1;
+        int missing = -1;
+
+        foreach (int num in nums)
+        {
+            if (!set.Add(num))
+            {
+                duplicate = num;
+            }
+        }
+
+        for (int i = 1; i <= nums.Length; i++)
+        {
+            if (!set.Contains(i))
+            {
+                missing = i;
+                break;
+            }
+        }
+
+        return new int[] { duplicate, missing };
+    }
+}
