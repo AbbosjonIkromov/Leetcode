6674cc5e337adbf453d8dcb563ceb983c9eaa96f
AbbosjonIkromov <abbos6562@gmail.com>
2026-03-15 21:32:55 +0500

    Solve ExcelSheetColumnNumber

---
+++ ExcelSheetColumnNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ExcelSheetColumnNumber;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        Test(solution, "AB"); // Output: 28
+        Test(solution, "ZY"); // Output: 701
+    }
+
+    static void Test(Solution solution, string columnTitle)
+    {
+        var result = solution.TitleToNumber(columnTitle);
+        Console.WriteLine($"[{result}]");
+    }
+}
+
+
+public class Solution
+{
+    public int TitleToNumber(string columnTitle)
+    {
+        int result = 0;
+        for (int i = 0; i < columnTitle.Length; i++)
+        {
+            result *= 26;
+            result += columnTitle[i] - 'A' + 1;
+        }
+
+        return result;
+    }
+}
