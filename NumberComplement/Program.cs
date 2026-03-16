4f24e335b45f6f3ac8e8f69bd28e19dae9629380
AbbosjonIkromov <abbos6562@gmail.com>
2026-03-16 09:29:00 +0500

    Solve NumberComplement

---
+++ NumberComplement/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace NumberComplement;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        Test(solution, 5); // Output: 2
+        Test(solution, 1); // Output: 0
+    }
+
+    static void Test(Solution solution, int num)
+    {
+        var result = solution.FindComplement(num);
+        Console.WriteLine($"[{result}]");
+    }
+}
+
+public class Solution
+{
+    public int FindComplement(int num)
+    {
+        var binaryLength = Convert.ToString(num, 2).Length;
+
+        int mask = (1 << binaryLength) - 1;
+
+        return num ^ mask;
+    }
+}
+
