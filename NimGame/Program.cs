a007dc9b90d50a90a2de59562a56c18327c81e42
AbbosjonIkromov <abbos6562@gmail.com>
2026-03-17 09:08:32 +0500

    Solve NimGame

---
+++ NimGame/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace NimGame;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        Test(solution, 1); // Output: [True]
+        Test(solution, 2); // Output: [True]
+    }
+
+    static void Test(Solution solution, int n)
+    {
+        var result = solution.CanWinNim(n);
+        Console.WriteLine($"[{result}]");
+    }
+}
+
+
+public class Solution
+{
+    public bool CanWinNim(int n)
+    {
+        return n % 4 != 0;
+    }
+}
