18d5a29d2cdb741c50e6bb56168f6a29add15c64
AbbosjonIkromov <abbos6562@gmail.com>
2026-03-19 09:25:19 +0500

    Solve ReverseLinkedList

---
+++ ReverseLinkedList/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace ReverseLinkedList;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        ListNode head = new ListNode(1,
+                            new ListNode(2,
+                            new ListNode(3,
+                            new ListNode(4,
+                            new ListNode(5)))));
+
+        Console.WriteLine("Original list:");
+        PrintList(head);
+
+        ListNode reversed = solution.ReverseList(head);
+
+        Console.WriteLine("Reversed list:");
+        PrintList(reversed);
+
+    }
+    static void PrintList(ListNode head)
+    {
+        ListNode current = head;
+        while (current != null)
+        {
+            Console.Write(current.val + " -> ");
+            current = current.next;
+        }
+        Console.WriteLine("null");
+    }
+}
+
+
+public class Solution
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        ListNode prev = null;
+        ListNode cur = head;
+
+        while (cur != null)
+        {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+
+        return prev;
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
