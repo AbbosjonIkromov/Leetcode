1c5f3e94919d1a009f9d017635540d99ce1b7a11
AbbosjonIkromov <abbos6562@gmail.com>
2026-03-20 22:02:24 +0500

    Solve LongestHarmoniousSubsequence

---
+++ LongestHarmoniousSubsequence/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace LongestHarmoniousSubsequence;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        Test(solution, new int[] { 1, 3, 2, 2, 5, 2, 3, 7 }); // output: [5]
+    }
+
+    static void Test(Solution solution, int[] nums)
+    {
+        var result = solution.FindLHS(nums);
+        Console.WriteLine($"[{result}]");
+    }
+}
+
+
+
+public class Solution
+{
+    public int FindLHS(int[] nums)
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        
+        foreach (int num in nums)
+        {
+            if (!dict.ContainsKey(num))
+                dict[num] = 0;
+
+            dict[num]++;
+        }
+
+        int maxLength = 0;
+
+        foreach (int key in dict.Keys)
+        {
+            if (dict.ContainsKey(key + 1))
+            {
+                int length = dict[key] + dict[key + 1];
+                maxLength = Math.Max(maxLength, length);
+            }
+        }
+
+        return maxLength;
+    }
+}
