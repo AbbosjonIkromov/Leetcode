d8d4f44aeaa36706bdb75228552dbbd121004a0d
abbosjonIkromov <abbos6562@gmail.com>
2026-04-02 09:38:31 +0500

    Solve DIStringMatch

---
+++ DIStringMatch/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace DIStringMatch;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+
+public class Solution
+{
+    public int[] DiStringMatch(string s)
+    {
+        int length = s.Length;
+        int[] result = new int[length + 1];
+        
+        for (int i = 0, low = 0, high = length; i <= length; i++)
+        {
+            if (i == length || s[i] == 'I')
+            {
+                result[i] = low++;
+            }
+            else
+            {
+                result[i] = high--;
+            }
+        }
+        return result;
+    }
+}
--- README.md
+++ README.md
@@ -6,6 +6,6 @@
 
 ## Progress
 
-* Easy: 91
+* Easy: 92
 * Medium: 0
 * Hard: 0
