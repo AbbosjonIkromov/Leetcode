21072f3a35aed174f336e2de429759ec9a5e29ac
AbbosjonIkromov <abbos6562@gmail.com>
2026-04-12 23:00:25 +0500

    Solve Maximum69Number

---
+++ Maximum69Number/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Maximum69Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            Test(solution, 9669); // output 9969
+        }
+
+        static void Test(Solution solution, int num)
+        {
+            var result = solution.Maximum69Number(num);
+            Console.WriteLine($"[{result}]");
+        }
+    }
+}
+
+
+public class Solution
+{
+    public int Maximum69Number(int num)
+    {
+        char[] digits = num.ToString().ToCharArray();
+        for (int i = 0; i < digits.Length; i++)
+        {
+            if (digits[i] == '6')
+            {
+                digits[i] = '9';
+                break;
+            }
+        }
+
+        return Convert.ToInt32(new string(digits));
+    }
+}
