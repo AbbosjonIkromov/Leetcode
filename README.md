e3743c27214e22cbe134502b0988ec4327117e5a
abbosjonIkromov <abbos6562@gmail.com>
2026-04-20 09:41:32 +0500

    Solve ReversePrefixOfWord

--- README.md
+++ README.md
@@ -6,6 +6,6 @@
 
 ## Progress
 
-* Easy: 106
+* Easy: 108
 * Medium: 0
 * Hard: 0
---
+++ ReversePrefixOfWord/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ReversePrefixOfWord;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        Test(solution, "abcdefd", 'd'); // output: dcbaefd
+        Test(solution, "xyxzxe", 'z'); // output: zxyxxe
+    }
+
+    static void Test(Solution solution, string word, char ch)
+    {
+        var result = solution.ReversePrefix(word, ch);
+        Console.WriteLine($"[{result}]");
+    }
+}
+
+
+public class Solution
+{
+    public string ReversePrefix(string word, char ch)
+    {
+        int index = word.IndexOf(ch);
+
+        if (index == -1)
+            return word;
+
+        char[] arr = word.ToCharArray();
+
+        int left = 0, right = index;
+
+        while (left < right)
+        {
+            (arr[left], arr[right]) = (arr[right], arr[left]);
+            left++;
+            right--;
+        }
+
+        return new string(arr);
+    }
+}
