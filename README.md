9dd785a36eae8104d9d6923929ce4000e2871563
abbosjonIkromov <abbos6562@gmail.com>
2026-04-21 10:48:31 +0500

    Solve SplitAStringInBalancedStrings

--- README.md
+++ README.md
@@ -6,6 +6,6 @@
 
 ## Progress
 
-* Easy: 108
+* Easy: 109
 * Medium: 0
 * Hard: 0
---
+++ SplitAStringInBalancedStrings/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace SplitAStringInBalancedStrings;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        Test(solution, "RLRRLLRLRL"); // output: 4
+        Test(solution, "RLRRRLLRLL"); // output: 2
+    }
+
+    static void Test(Solution solution, string s)
+    {
+        var result = solution.BalancedStringSplit(s);
+        Console.WriteLine($"[{result}]");
+    }
+}
+
+
+public class Solution
+{
+    public int BalancedStringSplit(string s)
+    {
+        int count = 0;
+        int result = 0;
+
+        foreach (var c in s)
+        {
+            if (c == 'R')
+                count++;
+            else
+                count--;
+
+            if (count == 0)
+                result++;
+        }
+
+        return result;
+    }
+}
