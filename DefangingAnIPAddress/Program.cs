8d9bf350b5d1e75b8e6215ac7cda1d5c4de38c79
abbosjonIkromov <abbos6562@gmail.com>
2026-04-30 14:20:31 +0500

    Solve DefangingAnIpAddress

---
+++ DefangingAnIPAddress/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DefangingAnIPAddress;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        Test(solution, "1.1.1.1"); // output: "1[.]1[.]1[.]1"
+        Test(solution, "255.100.50.0"); // output: "255[.]100[.]50[.]0"
+    }
+
+    static void Test(Solution solution, string address)
+    {
+        var result = solution.DefangIPaddr(address);
+        Console.WriteLine($"[{result}]");
+    }
+}
+
+
+public class Solution
+{
+    public string DefangIPaddr(string address)
+    {
+        return address.Replace(".", "[.]");
+    }
+}
