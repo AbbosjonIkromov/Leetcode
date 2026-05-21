a290a6299328144f554f80f86259449d0030664a
abbosjonIkromov <abbos6562@gmail.com>
2026-05-21 09:31:02 +0500

    Solve IncreasingDecreasingString

---
+++ IncreasingDecreasingString/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+
+namespace IncreasingDecreasingString;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        Test(solution, "aaaabbbbcccc"); // Output: "abccbaabccba"
+        Test(solution, "rat"); // Output : "art"
+    }
+
+    static void Test(Solution solution, string s)
+    {
+        var result = solution.SortString(s);
+        Console.WriteLine($"[{result}]");
+    }
+}
+
+public class Solution
+{
+    public string SortString(string s)
+    {
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            count[s[i] - 'a']++;
+        }
+
+        StringBuilder result = new StringBuilder();
+        while (result.Length < s.Length)
+        {
+            for (int i = 0; i < 26; i++)
+            {
+                if (count[i] > 0)
+                {
+                    result.Append((char)(i + 'a'));
+                    count[i]--;
+                }
+            }
+            for (int i = 25; i >= 0; i--)
+            {
+                if (count[i] > 0)
+                {
+                    result.Append((char)(i + 'a'));
+                    count[i]--;
+                }
+            }
+        }
+
+        return result.ToString();
+    }
+}
--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 ## Progress
 
-* Easy: 133
+* Easy: 134
 * Medium: 0
 * Hard: 0
 
