b475d10915ea42cf83a42a1eb1eb82e17fa9e6ed
AbbosjonIkromov <abbos6562@gmail.com>
2026-06-07 15:53:41 +0500

    Solve DivisorGame

---
+++ DivisorGame/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DivisorGame;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        Test(solution, 2); // output: true
+        Test(solution, 3); // output: false
+    }
+
+    static void Test(Solution solution, int n)
+    {
+        var result = solution.DivisorGame(n);
+        Console.WriteLine($"{result}");
+    }
+}
+
+
+public class Solution
+{
+    public bool DivisorGame(int n)
+    {
+        return n % 2 == 0;
+    }
+}
