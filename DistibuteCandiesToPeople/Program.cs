1b6e71137e3af952be1dfc833f041beaf81e3d8b
abbosjonIkromov <abbos6562@gmail.com>
2026-06-22 18:09:16 +0500

    Solve DistibutedCandiesToPeople

---
+++ DistibuteCandiesToPeople/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace DistibuteCandiesToPeople;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        Test(solution, 7, 4); // Output: [1, 2, 3, 1]
+        Test(solution, 10, 3); // Output: [5, 2, 3]
+    }
+
+    static void Test(Solution solution, int candies, int num_people)
+    {
+        var result = solution.DistributeCandies(candies, num_people);
+        Console.WriteLine($"[{string.Join(", ", result)}]");
+    }
+}
+
+
+public class Solution
+{
+    public int[] DistributeCandies(int candies, int num_people)
+    {
+        int[] distribution = new int[num_people];
+
+        int give = 1;
+        int index = 0;
+
+        while (candies > 0)
+        {
+            distribution[index] += Math.Min(give, candies);
+            candies -= give;
+            give++;
+            index = (index + 1) % num_people;
+        }
+
+        return distribution;
+    }
+}
--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 ## Progress
 
-* Easy: 153
+* Easy: 154
 * Medium: 0
 * Hard: 0
 
