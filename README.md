24b2324534f7a175489c346483514839ca359c83
Abbosjon Ikromov <abbos6562@gmail.com>
2026-08-21 09:28:55 +0500

    Solve RingsandRods

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 ## Progress
 
-* Easy: 182
+* Easy: 183
 * Medium: 0
 * Hard: 0
 
---
+++ RingsandRods/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace RingsandRods;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        Test(solution, "B0B6G0R6R0R6G9"); // Expected output: [1]
+        Test(solution, "B0R0G0R9R0B0G0"); // Expected output: [1]
+    }
+
+    static void Test(Solution solution, string rings)
+    {
+        int result = solution.CountPoints(rings);
+        Console.WriteLine($"[{result}]");
+    }
+}
+
+public class Solution
+{
+    public int CountPoints(string rings)
+    {
+        bool[,] rods = new bool[10, 3]; // 10 rods, 3 colors (R, G, B)
+
+        for (int i = 0; i < rings.Length; i += 2)
+        {
+            char color = rings[i];
+            int rodIndex = rings[i + 1] - '0';
+            switch (color)
+            {
+                case 'R':
+                    rods[rodIndex, 0] = true;
+                    break;
+                case 'G':
+                    rods[rodIndex, 1] = true;
+                    break;
+                case 'B':
+                    rods[rodIndex, 2] = true;
+                    break;
+            }
+        }
+
+        int result = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (rods[i, 0] && rods[i, 1] && rods[i, 2])
+            {
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
